b441b4132fa66a3f38a7454b1f26689bdeec48ad
jj <jjnilton@users.noreply.github.com>
2022-03-27 11:45:06 -0300

    adding spaces to tab format and removing first separator

--- init.el
+++ init.el
@@ -374,3 +374,37 @@ surrounded by word boundaries."
 (frameshot-setup
  '((name . "emacs")
    (output . "~/Downloads/")))
+
+
+;; Tab bar
+
+;; Overwrite emacs function to add left/right paddings
+(defun tab-bar-tab-name-current-with-count ()
+  "Generate tab name from the buffer of the selected window.
+Also add the number of windows in the window configuration."
+  (let ((count (length (window-list-1 nil 'nomini)))
+        (name (window-buffer (minibuffer-selected-window))))
+    (if (> count 1)
+        (format " %s (%d) " name count)
+      (format " %s " name))))
+
+
+;; Remove nth element of a list
+;; (defun remove-nth-element (nth list)
+;;   (if (zerop nth) (cdr list)
+;;     (let ((last (nthcdr (1- nth) list)))
+;;       (setcdr last (cddr last))
+;;       list)))
+;; Remove first tab separator
+;; (advice-add 'tab-bar-make-keymap-1 :around
+;;             (lambda (orig-fun)
+;;               (remove-nth-element 2 (funcall orig-fun))))
+
+
+;; Remove first tab separator (alternative)
+(advice-add 'tab-bar-make-keymap-1 :around
+            (lambda (orig-fun)
+                     (let ((l (funcall orig-fun)))
+                       (delq (setcar (nthcdr 2 l) (cons nil nil)) l))))
+
+
