c62920bf3ef15fbd0e54d3073153b65a118e528b
jj <jjnilton@users.noreply.github.com>
2022-05-02 22:38:35 -0300

    defer some packages' config

--- init.el
+++ init.el
@@ -55,11 +55,13 @@
 
 
 ;; Windsize & Windmove
-(windsize-default-keybindings)
-(global-set-key (kbd "C-M-<up>") 'windmove-up)
-(global-set-key (kbd "C-M-<left>") 'windmove-left)
-(global-set-key (kbd "C-M-<down>") 'windmove-down)
-(global-set-key (kbd "C-M-<right>") 'windmove-right)
+(with-eval-after-load 'windsize
+  (windsize-default-keybindings)
+  (global-set-key (kbd "C-M-<up>") 'windmove-up)
+  (global-set-key (kbd "C-M-<left>") 'windmove-left)
+  (global-set-key (kbd "C-M-<down>") 'windmove-down)
+  (global-set-key (kbd "C-M-<right>") 'windmove-right))
+
 ;; (global-set-key (kbd "C-x M-<up>") #'windmove-up)
 ;; (global-set-key (kbd "C-x M-<down>") #'windmove-down)
 ;; (global-set-key (kbd "C-x M-<left>") #'windmove-left)
@@ -68,29 +70,29 @@
 
 
 ;; Ivy/Swiper/Counsel
-(ivy-mode)
-(setq ivy-re-builders-alist
-      '((t . ivy--regex-fuzzy)))
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper-isearch)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+(with-eval-after-load 'ivy
+  (setq ivy-re-builders-alist
+        '((t . ivy--regex-fuzzy)))
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  ;; enable this if you want `swiper' to use it
+  ;; (setq search-default-mode #'char-fold-to-regexp)
+  (global-set-key "\C-s" 'swiper-isearch)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
 ;; Swiper-isearch with moving between lines with arrow keys
 (defun swiper-isearch-next-line ()
@@ -119,8 +121,9 @@
 
 
 ;; Projectile
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(with-eval-after-load 'projectile
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 
 ;; Custom Shortcuts
@@ -417,9 +420,11 @@ surrounded by word boundaries."
                        (if (buffer-file-name) (abbreviate-file-name (buffer-file-name)) "%b")))))
 
 ;; Frameshot
-(frameshot-setup
- '((name . "emacs")
-   (output . "~/Downloads/")))
+(with-eval-after-load 'frameshot
+  (frameshot-setup
+   '((name . "emacs")
+     (output . "~/Downloads/"))))
+
 
 
 ;; Tab bar
