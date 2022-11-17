60aecad183c4378199144066dcac15829326443d
JJ <jjnilton@users.noreply.github.com>
2022-11-17 18:55:22 -0300

    .plantuml file assoc.

--- init.el
+++ init.el
@@ -249,6 +249,7 @@
 (add-to-list 'auto-mode-alist '("\\.jinja\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.sql\\'" . sql-mode))
 (add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode))
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
 
 ;; Other
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
